4052b4213bc3f1c01fd932d473bd435405cc2395
Tural Hasanli <tural.hasanli@smoothstack.com>
2021-02-28 13:06:05 -0500

    adding password recovery/account creation

--- src/main/java/com/ss/utopia/repositories/UserRoleRepository.java
+++ src/main/java/com/ss/utopia/repositories/UserRoleRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 import com.ss.utopia.models.UserRole;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
